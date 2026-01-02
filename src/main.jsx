35d393ff71ceb967fa616c28cbd466744e3a85ca
Karthik <karthik@example.com>
2026-01-02 16:37:58 +0530

    Update Google Client ID

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
-      <GoogleOAuthProvider clientId="789922336469-s1mvfuf3ku7vqitdgq22jl137udqqipe.apps.googleusercontent.com">
+      <GoogleOAuthProvider clientId="789922336469-nhd2f31r6gl94e3g5ll60q5289aba4h0.apps.googleusercontent.com">
         <BrowserRouter>
           <App />
         </BrowserRouter>
